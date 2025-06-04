ac076b30252c289d22bf8102f7e69c91c0691813
Minh Dan <v.dannm2@vinbigdata.com>
2025-06-04 18:29:22 +0700

    update time

--- example/views.py
+++ example/views.py
@@ -1,15 +1,23 @@
 # example/views.py
 from datetime import datetime
+from pytz import timezone
 
 from django.http import HttpResponse
 
 def index(request):
-    now = datetime.now()
+    now = datetime.now(timezone('Asia/Ho_Chi_Minh'))
     html = f'''
     <html>
         <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
+            <h1>Hello world ! </h1>
+            <h2> About Me</h2>
+            <ul>
+                <li> Name: Nguyen Minh Dan </li>
+                <li> Age: 26 </li>
+                <li> Gender: Male </li>
+            </ul>
+            <h2> I did not done anything i thought this is good so i dont show for you </h2>
+            <p>The current time in Ho Chi Minh is { now }.</p>
         </body>
     </html>
     '''
